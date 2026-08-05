cfa776c1c171cc4b8ba4d5d613b04ffbd44b3e83
Mykyta Stelmashenko <mykytastelmashenko@gmail.com>
2026-08-05 10:35:22 +0300

    refactor: keep Home timeline focused

--- src/features/dreams/hooks/useHomeTimelineState.ts
+++ src/features/dreams/hooks/useHomeTimelineState.ts
@@ -1,72 +1,33 @@
 import React from 'react';
-import { type PatternDetailKind } from '../../../app/navigation/routes';
 import { type DreamCopy } from '../../../constants/copy/dreams';
 import { type AppLocale } from '../../../i18n/types';
 import {
   formatHeroDateLabel,
-  formatLastViewedDreamMeta,
-  formatResultCount,
   getContextGreeting,
   getHomeRevisitCue,
 } from '../model/homeOverview';
 import { getCurrentStreak } from '../model/dreamAnalytics';
 import { type Dream } from '../model/dream';
-import { isDreamArchived, sortDreamsNewestFirst } from '../model/dreamList';
-import {
-  getRecurringReflectionSignals,
-  getRecurringWordSignals,
-  getTranscriptArchiveStats,
-} from '../../stats/model/dreamReflection';
-
-const HOME_RECENT_LIMIT = 12;
+import { getHomeFeedState } from '../model/homeFeed';
 
 type UseHomeTimelineStateArgs = {
   dreams: Dream[];
   copy: DreamCopy;
   locale: AppLocale;
-  lastViewedDream: Dream | null;
 };
 
 export function useHomeTimelineState({
   dreams,
   copy,
   locale,
-  lastViewedDream,
 }: UseHomeTimelineStateArgs) {
-  const activeDreams = React.useMemo(
-    () =>
-      sortDreamsNewestFirst(dreams.filter(dream => !isDreamArchived(dream))),
-    [dreams],
-  );
-  const displayedDreams = React.useMemo(
-    () => activeDreams.slice(0, HOME_RECENT_LIMIT),
-    [activeDreams],
-  );
-
-  // Deferred analytics keep the recent-dream list responsive on large journals.
-  const deferredActiveDreams = React.useDeferredValue(activeDreams);
+  const feed = React.useMemo(() => getHomeFeedState(dreams), [dreams]);
+  const deferredActiveDreams = React.useDeferredValue(feed.activeItems);
 
   const streak = React.useMemo(
-    () => getCurrentStreak(activeDreams),
-    [activeDreams],
+    () => getCurrentStreak(feed.activeItems),
+    [feed.activeItems],
   );
-  const spotlightWord = React.useMemo(
-    () => getRecurringWordSignals(deferredActiveDreams, 1)[0],
-    [deferredActiveDreams],
-  );
-  const spotlightTheme = React.useMemo(
-    () => getRecurringReflectionSignals(deferredActiveDreams, { limit: 1 })[0],
-    [deferredActiveDreams],
-  );
-  const transcriptArchiveStats = React.useMemo(
-    () => getTranscriptArchiveStats(deferredActiveDreams),
-    [deferredActiveDreams],
-  );
-  const moodBacklogCount = React.useMemo(
-    () => deferredActiveDreams.filter(dream => !dream.mood).length,
-    [deferredActiveDreams],
-  );
-
   const heroGreeting = React.useMemo(() => getContextGreeting(copy), [copy]);
   const heroDateLabel = React.useMemo(
     () => formatHeroDateLabel(locale),
@@ -76,57 +37,13 @@ export function useHomeTimelineState({
     () => getHomeRevisitCue(deferredActiveDreams, copy),
     [copy, deferredActiveDreams],
   );
-  const lastViewedDreamMeta = React.useMemo(
-    () => formatLastViewedDreamMeta(lastViewedDream, copy, locale),
-    [copy, lastViewedDream, locale],
-  );
-
-  const spotlightPattern =
-    spotlightWord?.label ??
-    spotlightTheme?.label ??
-    copy.homeSpotlightNoPattern;
-  const spotlightPatternKind: PatternDetailKind | null = spotlightWord
-    ? 'word'
-    : spotlightTheme
-      ? 'theme'
-      : null;
-  const spotlightCountLabel = React.useMemo(
-    () =>
-      formatResultCount(
-        (spotlightWord?.dreamCount ?? spotlightTheme?.dreamCount) || 0,
-        copy,
-      ),
-    [copy, spotlightTheme?.dreamCount, spotlightWord?.dreamCount],
-  );
-  const attentionValue =
-    transcriptArchiveStats.audioOnly > 0
-      ? transcriptArchiveStats.audioOnly === 1
-        ? copy.homeSpotlightAttentionAudioSingle
-        : `${transcriptArchiveStats.audioOnly} ${copy.homeSpotlightAttentionAudioPlural}`
-      : moodBacklogCount > 0
-        ? moodBacklogCount === 1
-          ? copy.homeSpotlightAttentionMoodSingle
-          : `${moodBacklogCount} ${copy.homeSpotlightAttentionMoodPlural}`
-        : copy.homeSpotlightAttentionClear;
-  const attentionHint =
-    transcriptArchiveStats.audioOnly > 0
-      ? copy.homeSpotlightAttentionAudioHint
-      : moodBacklogCount > 0
-        ? copy.homeSpotlightAttentionMoodHint
-        : copy.homeSpotlightAttentionClearHint;
 
   return {
-    activeDreamCount: activeDreams.length,
-    displayedDreams,
+    activeDreamCount: feed.activeCount,
+    displayedDreams: feed.recentItems,
     heroGreeting,
     heroDateLabel,
     revisitCue,
-    lastViewedDreamMeta,
     streak,
-    spotlightPattern,
-    spotlightPatternKind,
-    spotlightCountLabel,
-    attentionValue,
-    attentionHint,
   };
 }
